58e42f47b69dd1a67bfaace52c657a6a8cbded21
Noah Schairer <nschairer@gmail.com>
2018-08-02 00:05:10 -0400

    Started python repository

--- JavaScript/decompressFile.js
+++ JavaScript/decompressFile.js
@@ -57,4 +57,4 @@ function readFile(name) {
 	});
 }
 
-readFile('binaryfile');
+readFile('binaryFile');
---
+++ Python/HuffmanEncoder.py
@@ -0,0 +1,56 @@
+from node import Node
+
+def frequencyMap(text):
+	map = {}
+	for char in text:
+		if char in map:
+			map[char] += 1
+		else:
+			map[char] = 1
+	return map
+
+def sortAndCreateNodes(freqMap):
+	sortable = []
+	for obj in freqMap:
+		sortable.append([obj, freqMap[obj]])
+	sortable.sort(key=lambda x: x[1])
+	nodes = list(map(Node, sortable))
+	return nodes
+
+def minHeap(nodes):
+	while len(nodes) >= 2:
+		one = nodes.pop(0)
+		two = nodes.pop(0)
+		total = one.data[1] + two.data[1]
+		if one.data[1] <= two.data[1]:
+			nodes.append(Node(['',total], one, two))
+		else:
+			nodes.append(Node(['',total], two, one))
+		nodes.sort(key=lambda x: x.data[1])
+	return nodes[0]
+
+def mapPrefixes(map, root, path=''):
+	if root.left is None and root.right is None:
+		map[root.data[0]] = path
+	else:
+		mapPrefixes(map, root.left, path+'0')
+		mapPrefixes(map, root.right, path+'1')
+
+def generatePrefixes(tree):
+	prefixes = {}
+	mapPrefixes(prefixes, tree)
+	print(prefixes)
+	return prefixes
+
+def encodeString(text):
+	result = ''
+	freqMap = frequencyMap(text)
+	tree = minHeap(sortAndCreateNodes(freqMap))
+	prefixes = generatePrefixes(tree)
+	for char in text:
+		for code in prefixes:
+			if char == code:
+				result += prefixes[code]
+	print(result)
+
+encodeString('AAAAAAABBBC')
